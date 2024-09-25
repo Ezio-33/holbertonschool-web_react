3f5b1e3e1dbde6029fcb296327edf89e7dd3bde5
Ezio-33 <8691@holbertonstudents.com>
2024-09-26 00:40:29 +0200

    task 6

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -1,4 +1,4 @@
-// Définition de l'interface Teacher
+// Interface pour les enseignants
 interface Teacher {
   firstName: string;
   lastName: string;
@@ -8,17 +8,78 @@ interface Teacher {
   [propName: string]: any;
 }
 
-// Définition de l'interface Directors qui étend Teacher
-interface Directors extends Teacher {
+// Interface pour les directeurs qui étend Teacher
+interface Director extends Teacher {
   numberOfReports: number;
 }
 
-const director1: Directors = {
-  firstName: "John",
-  lastName: "Doe",
-  location: "London",
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
+// Interface pour les méthodes des directeurs
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
 
-console.log(director1);
+// Interface pour les méthodes des enseignants
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Classe Director implémentant les méthodes nécessaires
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+// Classe Teacher implémentant les méthodes nécessaires
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+// Fonction pour créer un employé
+function createEmployee(salary: number | string): Teacher | Director {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+// Fonction pour vérifier si un employé est un directeur
+function isDirector(employee: Teacher | Director): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// Fonction pour exécuter le travail d'un employé
+function executeWork(employee: Teacher | Director): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+
+// Exemples d'utilisation
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
